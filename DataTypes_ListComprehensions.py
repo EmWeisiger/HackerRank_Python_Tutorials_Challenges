0d6844de303eb4ea5d9e1d5a60422bb1a415a215
Em Weisiger <emweisiger@gmail.com>
2016-01-25 12:08:22 -0700

    Create DataTypes_ListComprehensions.py

---
+++ DataTypes_ListComprehensions.py
@@ -0,0 +1,4 @@
+# HackerRank_Python_Tutorials_Challenges > Data Types > List Comprehensions
+
+x, y, z, n = (int(input()) for _ in range(4))
+print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])
