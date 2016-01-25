8d461096328c0fa3e6299bffa7dacf89e9821dfb
Em Weisiger <emweisiger@gmail.com>
2016-01-25 12:01:41 -0700

    Create Introduction_WhatsYourName.py

---
+++ Introduction_WhatsYourName.py
@@ -0,0 +1,6 @@
+# HackerRank_Python_Tutorials_Challenges > Introduction > What's Your Name?
+
+firstname = raw_input()
+lastname = raw_input()
+
+print ("Hello " + firstname + " " + lastname + "! You just delved into python.")
