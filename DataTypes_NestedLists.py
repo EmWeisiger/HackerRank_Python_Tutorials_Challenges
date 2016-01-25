6dea56e7e82261f6fcb5b566febbd93513fc474b
Em Weisiger <emweisiger@gmail.com>
2016-01-25 12:11:20 -0700

    Create DataTypes_NestedLists.py

---
+++ DataTypes_NestedLists.py
@@ -0,0 +1,7 @@
+# HackerRank_Python_Tutorials_Challenges > Data Types > Nested Lists
+
+n = int(raw_input())
+marks = [[raw_input(), float(raw_input())] for i in  xrange(n)]           
+scores = sorted({s[1] for s in marks})
+result = sorted(s[0] for s in marks if s[1] == scores[1])
+print '\n'.join(result)
